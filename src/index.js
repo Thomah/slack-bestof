0108b997df900cd1d035dfa7b363e468f38f24f6
Thomas HINGANT <thomas.hingant@cnieg.fr>
2019-11-22 17:01:48 +0100

    Debug in full recursive mode

--- src/index.js
+++ src/index.js
@@ -36,40 +36,45 @@ function getHistory(channel, limit, latest) {
     (async() => {
         var res = await web.channels.history({ channel: channel, latest: latest });
         if (res.ok) {
-            var message;
-            var date;
-            var dateReach = false;
-            var k = 0;
-            while (!dateReach && k < res.messages.length) {
-                message = res.messages[k];
-                date = new Date(message.ts * 1000);
-                dateReach = date.getTime() < limit.getTime();
-                if (!dateReach && message.reactions != undefined) {
-                    message.nbReactions = 0;
-                    console.log(message.reactions);
-                    for (var i = 0; i < message.reactions.length; i++) {
-                        message.nbReactions += message.reactions[i].count;
-                    }
-                    db.insert("messages", message, (data) => {
-                        var reactions = message.reactions;
-                        logger.log(`extractReactions(${data.insertedId}, ${message.reactions}, 0)`);
-                        extractReactions(data.insertedId, reactions, 0);
-                    });
-                }
-                k++;
-            }
-            //logger.log("Emojis");
-            //logger.log(JSON.stringify(emojis));
-            if (date.getTime() >= limit.getTime()) {
+            extractMessages(res.messages, limit, 0, () => {
                 setTimeout(function() {
-                    getHistory(channel, limit, res.messages[k - 1].ts);
+                    getHistory(channel, limit, res.messages[messages.length - 1].ts);
                 }, 1000);
-            }
+            });
         }
     })();
 }
 
-function extractReactions(messageId, reactions, numReaction) {
+function extractMessages(messages, limit, index, callback) {
+    if (index < messages.length) {
+        message = messages[index];
+        var date = new Date(message.ts * 1000);
+        var dateReach = false;
+        dateReach = date.getTime() < limit.getTime();
+        if (!dateReach && message.reactions != undefined) {
+            logger.log("Date limite non atteinte et réactions présentes");
+            message.nbReactions = 0;
+            for (var i = 0; i < message.reactions.length; i++) {
+                message.nbReactions += message.reactions[i].count;
+            }
+            db.insert("messages", message, (data) => {
+                var reactions = message.reactions;
+                extractReactions(data.insertedId, reactions, 0, () => {
+                    extractMessages(messages, limit, index + 1, callback);
+                });
+            });
+        } else if (!dateReach) {
+            logger.log("Date limite non atteinte");
+            extractMessages(messages, limit, index + 1, callback);
+        }
+    } else {
+        logger.log("Fin des messages de la liste");
+        callback();
+    }
+}
+
+function extractReactions(messageId, reactions, numReaction, callback) {
+    logger.log(`Extraction des réactions : ${messageId} : ${numReaction} / ${reactions.length}`);
     if (reactions != undefined && numReaction < reactions.length) {
         var reaction = reactions[numReaction];
         db.read("emojis", { name: reaction.name }, function(emoji) {
@@ -80,8 +85,7 @@ function extractReactions(messageId, reactions, numReaction) {
                     usage: reaction.count
                 }
                 db.insert("emojis", entry, () => {
-                    logger.log(`extractReactions(${messageId}, ${reactions}, ${numReaction + 1})`);
-                    extractReactions(messageId, reactions, numReaction + 1);
+                    extractReactions(messageId, reactions, numReaction + 1, callback);
                 })
             } else {
                 var entry = {
@@ -90,10 +94,11 @@ function extractReactions(messageId, reactions, numReaction) {
                     usage: emoji.usage + reaction.count
                 }
                 db.update("emojis", { name: reaction.name }, entry, () => {
-                    logger.log(`extractReactions(${messageId}, ${reactions}, ${numReaction + 1})`);
-                    extractReactions(messageId, reactions, numReaction + 1);
+                    extractReactions(messageId, reactions, numReaction + 1, callback);
                 })
             }
         });
+    } else {
+        callback();
     }
 }
